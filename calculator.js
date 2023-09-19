f9472e947be5bee8121cc48a120ce4db4947352a
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2023-09-19 14:20:50 -0500

    Update calculator.js
    updated symbol function to do chaining arithmetic and fixed calculator to be used more than once. 
    added notes for code

--- calculator.js
+++ calculator.js
@@ -1,73 +1,70 @@
-function Key(button,element,value){
-	this.button=button;
-  this.element= element;
-  this.value= value;
+function Key(button,element,value){ //creating object values
+	this.button=button; // correlates to each button on screen 
+  this.element= element; //links button element to object
+  this.value= value; // adds integar or symbol string value to object variable
 }
+//list of variables in Key function in their empty state
 let firstNumber=[];
 let secondNumber =[];
 let numberSwitch=false;
+let firstInt=0;
+let secondInt=0;
+let total= 0;
 Key.prototype ={
 	numberOutput: function (){
-		(this.element).onclick=()=>{
-    outputBar.textContent+=this.value;
-    if (numberSwitch==false){
+		(this.element).onclick=()=>{ //number button is clicked
+    outputBar.textContent+=this.value;//the number is appended to the end of the chain of number in output bar
+    if (numberSwitch==false){//this denotes if this is the number before or after a symbole is picked 
       firstNumber.push(this.value);
       console.log(`first number is ${firstNumber}`);
       }
     else secondNumber.push(this.value);
-    console.log(`second number is ${secondNumber}`);
+    		console.log(`second number is ${secondNumber}`);
     }
   }, 
-  symbolOutput: function (){
+  symbolOutput: function (){//function for when a symbole is picked
 		(this.element).onclick=()=>{
-    outputBar.textContent=this.value;
-    if (this.value == '+'){
-          firstInt=Number(firstNumber.join(""))
-          console.log(firstInt);
-          symbol=this.value
-          firstNumber=[];
-          numberSwitch=true;
+    outputBar.textContent=this.value;//output bar clears and only symbol is shown until 2nd number started
+    if (this.value == '+'||this.value == '-'||this.value == 'x'||this.value =='/'){
+    	if(firstInt==0){//if symbol is not enter or clear and first number hasn't been established
+          firstInt=Number(firstNumber.join(""))//the string of numbers becomes an integer from the array
+          console.log(`firstInt=${firstInt} secondInt= ${secondInt}`);//debugging section
+          symbol=this.value//symbol variable becomes == to the symbol that was chosen
+          firstNumber=[];// firstNubmer array reset to empty 
+          numberSwitch=true;//switch turned on to make new number buttons chain to the 2nd number
+          }
+       if(firstInt!=0||secondInt!=0){//for when enter has been pressed
+       	  secondInt=Number(secondNumber.join(""))//2nd integer is made from 2nd number array
+          console.log(`firstInt=${firstInt} secondInt= ${secondInt} numberSwitch is ${numberSwitch} `);
+          symbol=this.value// used if doing math on total
+          secondNumber=[];//secondNumber array reset
+          
     			}
-     if (this.value == '-'){
-     			firstInt=Number(firstNumber.join(""))
-          console.log(firstInt);
-          symbol=this.value
-          firstNumber=[];
-          numberSwitch=true;
-    			}
-      if (this.value == 'x'){
-          firstInt=Number(firstNumber.join(""))
-          console.log(firstInt);
-          symbol=this.value
-          firstNumber=[];
-          numberSwitch=true;
-    			}
-       if (this.value =='/'){
-       		firstInt=Number(firstNumber.join(""))
-          console.log(firstInt);
-          symbol=this.value
-          firstNumber=[];
-          numberSwitch=true;
-    			}
-        if (this.value == ' '){
+       }     
+        if (this.value == ' '){//clear button resets everything to beginning values again
         	firstNumber= [];
           secondNumber=[];
           firstInt=0;
           secondInt=0;
+          numberSwitch=false;
         }
-        if (this.value == '='){
-        	secondInt=Number(secondNumber.join(""));
-          if(symbol=='+'){total=firstInt+secondInt}
+        if (this.value == '='){// for enter
+        
+        	if(numberSwitch==true){	secondInt=Number(secondNumber.join(""));}//used if it hasn't happened yet 
+          //actual math turning symbol into what is being performed
+          if(symbol == '+'){total=firstInt+secondInt}
           if(symbol == '-'){total=firstInt-secondInt}
           if(symbol == 'x'){total = firstInt*secondInt}
           if(symbol == '/'){total = firstInt/secondInt}
-          outputBar.textContent=(total)
+          outputBar.textContent=(total)//total shown in output bar
           
-          console.log(total)
-          secondNumber=[];
-          firstInt=0;
+          console.log(total, symbol)
+          
+          secondNumber=[];//secondnumber array reset again probably redundant
+          firstInt=total;//if not being cleared, math performed on total w/ new button clicks forming 2nd number
           secondInt=0;
-          numberSwitch=false;
+          symbol =''
+          
         	}
        }
 		},
@@ -108,5 +105,3 @@ const enter= new Key('enter', document.getElementById('enter-button'), '=')
 enter.symbolOutput();
 
 const outputBar= document.getElementById('output-input');
-
-
